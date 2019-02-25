88e29824d07294e788bdb4d215b37ce8b16ab4c7
Mateusz Magda <realgs@gmail.com>
2019-02-25 12:18:39 +0100

    Delete About component, clean up project

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,5 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">spacer</router-link> |
-      <router-link to="/about">about</router-link>
-    </div>
     <router-view/>
   </div>
 </template>
